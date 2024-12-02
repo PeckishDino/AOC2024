0479168df5886f4ad641d34d0f30e29eb4cab3a3
PeckishDino <peckishdino@hotmail.com>
2024-12-02 20:12:53 +0000

    day 2 part 1 only

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -23,32 +23,30 @@ def one():
         print(valid_levels)
 
 
-
-
 def two():
     valid_levels = 0
-
     with open("input.txt", "r") as file:
         for line in file:
             status = None
-            levels = line.split()
-            print(levels)
-            ascending = int(levels[0]) - int(levels[1])  # if the initial 2 values are ascending, descending,
+            levels = line.strip().split()
+            length = len(levels)
+            ascend_or_descend = int(levels[0]) - int(levels[1])  # if the initial 2 values are ascending, descending,
             # or no difference.
-            if ascending < 0:
-                status is True
-            for x in range(len(levels)):
-                if status is True and levels[x] - levels[x+1] >= 0:
-                    print("No longer ascending")
+            if ascend_or_descend > 0:
+                status = True
+            if ascend_or_descend < 0:
+                status = False
+            for x in range(length):
+                if x + 1 == length:  # if it reaches the end of the level aka reads every number
+                    valid_levels += 1
                     break
-
-
-
-
+                ascend_or_descend2 = int(levels[x]) - int(levels[x + 1])
+                if status is False and ascend_or_descend2 > 0 or status is True and ascend_or_descend2 < 0 or status is None or abs(
+                        ascend_or_descend2) < 1 or abs(ascend_or_descend2) > 3:
+                    break  # breaks if the increment is 0, or it switches from ascend to descend or vice versa
 
         print(valid_levels)
 
 
 # one()
 two()
-
