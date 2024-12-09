3133cce6a928e14cf647863e634d06eead874efd
PeckishDino <peckishdino@hotmail.com>
2024-12-09 21:36:17 +0000

    Update Day 9.py

--- Day 9/Day 9.py
+++ Day 9/Day 9.py
@@ -1,5 +1,5 @@
 def readfile(file):
-    with open(file,"r") as f:
+    with open(file, "r") as f:
         return f.readlines()
 
 
@@ -22,7 +22,7 @@ def one():
         start += 1
     print(string_list)
     length = len(string_list)
-    #print(length)
+    # print(length)
     z = length - 1
     while z > 0 and "." in string_list:
         closest_dot = string_list.index(".")
@@ -33,9 +33,36 @@ def one():
         z -= 1
     print(string_list)
 
-
-    for i,_ in enumerate(string_list):
+    for i, _ in enumerate(string_list):
         if _ != ".":
-            sum += i*_
-    print("sum:",sum)
-one()
+            sum += i * _
+    print("sum:", sum)
+
+
+#one()
+
+
+def two():
+    ran_through = set()
+    sum = 0
+    string_list = []
+    f = readfile("input.txt")
+    string = f[0]
+    start = 0
+    id = 0
+    for c in string:
+        if (start % 2) == 0:
+            for _ in range(int(c)):
+                string_list.append(id)
+        else:
+            for _ in range(int(c)):
+                string_list.append(".")
+        if string_list[-1] != "." and c != "0":
+            id += 1
+        start += 1
+    print(string_list)
+    length = len(string_list)
+    print(length)
+
+
+two()
