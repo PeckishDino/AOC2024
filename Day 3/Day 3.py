f2b44bf4837332c4cbd9c85dbe1ab5a9f4086737
PeckishDino <peckishdino@hotmail.com>
2024-12-14 19:53:38 +0000

    functions

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -1,34 +1,62 @@
 import re
 
+pattern = r"mul\(\d+,\d+\)"
+pattern2 = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+d = []
+y = []
 
-def one():
+
+def parse(file):
+    with open(file, "r") as f:
+        d.extend([line for line in f])
+    return d
+
+
+def read_line(entry):
+    for x in entry:
+        y.extend(re.findall(pattern, x))
+    return y
+
+def read_line2(entry):
+    for x in entry:
+        y.extend(re.findall(pattern2, x))
+    return y
+
+
+def iterate_through(values):
     sum = 0
-    p = r"mul\(\d+,\d+\)"
-    with open("input.txt", "r") as file:
-        for line in file:
-            x = re.findall(p, line)
-            for y in x:
-                nums = re.findall(r"\d+", y)
-                sum += int(nums[0]) * int(nums[-1])
-    print(sum)
+    print(values)
+    for x in values:
+        nums = re.findall(r"\d+", x)
+        print(nums, nums[0], nums[-1])
+        sum += int(nums[0]) * int(nums[-1])
+    return sum
 
-def two():
+
+def one_functions():
+    d.clear()
+    y.clear()
+    print(iterate_through(read_line(parse("input.txt"))))
+
+
+one_functions()
+
+def iterate_through2(values):
     sum = 0
     status = True
-    p = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
-    with open("input.txt", "r") as file:
-        for line in file:
-            x = re.findall(p, line)
-            print(x)
-            for y in x:
-                if y == "don't()":
-                    status = False
-                elif y == "do()":
-                    status = True
-                elif status:
-                    nums = re.findall(r"\d+", y)
-                    sum += int(nums[0]) * int(nums[-1])
-    print(sum)
+    for x in values:
+        if x == "don't()":
+            status = False
+        elif x == "do()":
+            status = True
+        elif status:
+            nums = re.findall(r"\d+", x)
+            sum += int(nums[0]) * int(nums[-1])
+    return sum
 
-one()
-two()
+def two_functions():
+    d.clear()
+    y.clear()
+    print(iterate_through2(read_line2(parse("input.txt"))))
+
+two_functions()
