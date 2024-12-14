36daef0f575fe02b681c8d9333addd2896c423ad
PeckishDino <peckishdino@hotmail.com>
2024-12-14 20:23:03 +0000

    functions

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,33 +1,29 @@
-def one():
-    sum=0
-    left=[]
-    right=[]
-    with open("input.txt","r") as file:
-        for line in file:
+def parse(file):
+    global left, right
+    left,right = [],[]
+    with open(file, "r") as f:
+        for line in f:
             x = line.strip().split()
             left.append(x[0])
             right.append(x[-1])
     left.sort()
     right.sort()
+
+
+def one():
+    sum = 0
+    parse("input.txt")
     for x in range(len(left)):
-        sum+=abs(int(left[x])-int(right[x]))
+        sum += abs(int(left[x]) - int(right[x]))
     print(sum)
 
 
 def two():
-    sum=0
-    left=[]
-    right=[]
-    with open("input.txt", "r") as file:
-        for line in file:
-            x = line.strip().split()
-            left.append(x[0])
-            right.append(x[-1])
-    left.sort()
-    right.sort()
+    sum = 0
+    parse("input.txt")
     for x in range(len(left)):
         similarity = right.count(left[x])
-        sum+=(int(left[x])*similarity)
+        sum += (int(left[x]) * similarity)
     print(sum)
 
 
